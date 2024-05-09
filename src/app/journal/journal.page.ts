bf6e1ea857a6858ab8220f4f91c9d5561a8cb2db
JosephShortt <g00419690@atu.ie>
2024-05-09 01:06:22 +0100

    Added journal page

--- src/app/journal/journal.page.ts
+++ src/app/journal/journal.page.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonList, IonItem, IonLabel, IonButton, IonCard, IonCardContent, IonIcon , IonCardHeader, IonCardTitle,IonGrid,IonRow,IonCol,IonInput, IonTextarea} from '@ionic/angular/standalone'; // Adjust the import paths if necessary
+
+interface JournalEntry {
+  country: string;
+  date: string;
+  notes: string;
+}
 
 @Component({
   selector: 'app-journal',
   templateUrl: './journal.page.html',
   styleUrls: ['./journal.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule,IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonList, IonItem, IonLabel, IonButton, IonCard, IonCardContent, IonIcon , IonCardHeader, IonCardTitle,IonGrid,IonRow,IonCol,IonInput, IonTextarea ]
 })
 export class JournalPage implements OnInit {
+  journalEntry: JournalEntry = { country: '', date: new Date().toISOString(), notes: '' };
+  journalEntries: JournalEntry[] = [];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  updateField(field: keyof JournalEntry, value: string) {
+    this.journalEntry[field] = value;
+  }
+  
+
+  addEntry() {
+    this.journalEntries.push({...this.journalEntry});
+    this.journalEntry = { country: '', date: new Date().toISOString(), notes: '' }; // Reset form
+  }
 }
